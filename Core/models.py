b40fec52b8b0dc8744c9de62badbed3ea2201ccb
Destiny Ashley <guilbertashley84@gmail.com>
2025-06-18 15:59:31 -0600

    BaseDeDatos

--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Curso (models.Model):
+    nombre = models.CharField(max_length=9)
+    camada = models.IntegerField()
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=9)
+    Apellido = models.CharField(max_length=18)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=9)
+    Apellido = models.CharField(max_length=18)
+    email = models.EmailField()
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=9)
+    fechaEntrega = models.DateField()
+    entregado = models.BooleanField()
+    
 # Create your models here.
